3db33fcc841b6441f18ffa2a1b99463f2d645b57
Vinícius das Chagas Silva <vinimaster@gmail.com>
2010-12-13 17:22:22 -0200

    Fixed wrong browser startup

--- project/features/project_steps.py
+++ project/features/project_steps.py
@@ -3,31 +3,32 @@ from nose.tools import assert_equals
 from splinter.browser import Browser
 from should_dsl import should, should_not
 
-
-browser = Browser()
-        
+@before.all
+def set_browser():
+    world.browser = Browser()
+   
 @step(r'I am on the new project page')
 def project_page(step):
-    browser.visit('http://localhost:8000/project/new')
+    world.browser.visit('http://localhost:8000/project/new')
     
 @step(r'I fill in "(.*)" with "(.*)"')
 def fill_field(step, label, value):
-    browser.fill_in(label, value)
+    world.browser.fill_in(label, value)
     
 @step(r'I press "(.*)"')
 def press_button(step, name):
-    browser.find_by_name(name.lower()).click()
+    world.browser.find_by_name(name.lower()).click()
     
 @step(r'I should see "(.*)": "(.*)"')
 def see_data_project(step, label, value):
-    tag = browser.find_by_name(label.lower()).value
+    tag = world.browser.find_by_name(label.lower()).value
     result = "%s: %s" % (label, value)
     tag |should| equal_to(result)
     
 @after.all
 def finish_him(total_result):
-    browser.quit()
+    world.browser.quit()
     print "Congratulations, %d of %d scenarios passed!" % (
-        total_result.scenarios_ran,
-        total_result.scenarios_passed
+        total_result.scenarios_passed,
+        total_result.scenarios_ran
     )
