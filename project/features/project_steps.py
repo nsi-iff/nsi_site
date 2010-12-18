4f0fe66c757b52f7ef3208a07fb8c3727121e91a
Vinícius das Chagas Silva <vinimaster@gmail.com>
2010-12-18 11:23:00 -0200

    refactoring to use zope.testbrowser instead of webdriver to make the tests more faster

--- project/features/project_steps.py
+++ project/features/project_steps.py
@@ -7,22 +7,22 @@ from project.models import Project
 @before.all
 def set_browser():
     world.browser = Browser()
-   
+
 @step(r'I am on the new project page')
 def project_creation_page(step):
     world.browser.visit('http://localhost:8000/project/new')
-    
+
 @step(r'I fill in "(.*)" with "(.*)"')
 def fill_field(step, label, value):
     world.browser.fill_in(label, value)
-    
+
 @step(r'I press "(.*)"')
 def press_button(step, name):
     world.browser.find_by_name(name.lower()).click()
-    
+
 @step(r'I should see "(.*)": "(.*)"')
 def see_data_project(step, label, value):
-    tag = world.browser.find_by_name(label.lower()).value
+    tag = world.browser.find_by_id(label.lower()).value
     result = "%s: %s" % (label, value)
     tag |should| equal_to(result)
 
@@ -34,10 +34,12 @@ def project_in_database(step):
 
 @step(r'I am on the "(.*)" project edit page')
 def project_update_page(step, project_name):
+    project_name = project_name.replace(' ','%20')
     world.browser.visit('http://localhost:8000/project/update/'+project_name+'/')
-    
+
 @step(r'I am on the "(.*)" project delete page')
 def project_delete_page(step, project_name):
+    project_name = project_name.replace(' ','%20')
     world.browser.visit('http://localhost:8000/project/delete/'+project_name+'/')
 
 @step(r'I should see the message "(.*)"')
@@ -55,7 +57,7 @@ def clear_database(scenario):
     projects = Project.objects.all()
     for project in projects:
         project.delete()
-    
+
 @after.all
 def finish_him(total_result):
     world.browser.quit()
@@ -63,3 +65,4 @@ def finish_him(total_result):
         total_result.scenarios_passed,
         total_result.scenarios_ran
     )
+
