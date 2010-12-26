9e85e22f6a46f79f0dcb545eb3cdb5c4a58f3563
Rodrigo Manhães <rmanhaes@gmail.com>
2010-12-26 16:54:46 -0200

    removed duplicate code from step definitions

--- project/features/project_steps.py
+++ project/features/project_steps.py
@@ -4,27 +4,9 @@ from splinter.browser import Browser
 from should_dsl import should
 from project.models import Project
 
-@before.all
-def set_browser():
-    world.browser = Browser('zope.testbrowser')
-
 @step(r'I am on the new project page')
 def project_creation_page(step):
     world.browser.visit('http://localhost:8000/project/new')
-
-@step(r'I fill in "(.*)" with "(.*)"')
-def fill_field(step, label, value):
-    world.browser.fill_in(label, value)
-
-@step(r'I press "(.*)"')
-def press_button(step, name):
-    world.browser.find_by_name(name.lower()).click()
-
-@step(r'I should see "(.*)": "(.*)"')
-def see_data_project(step, label, value):
-    tag = world.browser.find_by_id(label.lower()).value
-    result = "%s: %s" % (label, value)
-    tag |should| equal_to(result)
 
 @step('I have the following project')
 def project_in_database(step):
@@ -42,27 +24,8 @@ def project_delete_page(step, project_name):
     project_name = project_name.replace(' ','%20')
     world.browser.visit('http://localhost:8000/project/delete/'+project_name+'/')
 
-@step(r'I should see the message "(.*)"')
-def see_delete_project_message(step, value):
-    tag = world.browser.find_by_css_selector('h1').value
-    tag |should| equal_to(value)
-
 @step(r'the "(.*)" project does not exist')
 def project_does_not_exist(step, project_name):
     project = Project.objects.filter(name=project_name)
     len(project) |should| equal_to(0)
-
-@after.each_scenario
-def clear_database(scenario):
-    projects = Project.objects.all()
-    for project in projects:
-        project.delete()
-
-@after.all
-def finish_him(total_result):
-    world.browser.quit()
-    print "Congratulations, %d of %d scenarios passed!" % (
-        total_result.scenarios_passed,
-        total_result.scenarios_ran
-    )
 
---
+++ terrain.py
@@ -0,0 +1,55 @@
+from selenium.firefox.firefox_profile import FirefoxProfile
+from splinter.browser import Browser
+from lettuce import after, before, world, step
+from should_dsl import should
+from news.models import News
+from project.models import Project
+
+@before.all
+def set_browser():
+    enable_selenium_specs_to_run_offline()
+    world.browser = Browser()
+
+def enable_selenium_specs_to_run_offline():
+    prefs = FirefoxProfile._get_webdriver_prefs()
+    prefs['network.manage-offline-status'] = 'false'
+    @staticmethod
+    def prefs_func():
+        return prefs
+    FirefoxProfile._get_webdriver_prefs = prefs_func
+
+@after.each_scenario
+def clear_database(scenario):
+    for klass in (Project, News):
+        for obj in klass.objects.all():
+            obj.delete()
+
+@after.all
+def finish_him(total_result):
+    world.browser.quit()
+    print "Congratulations, %d of %d scenarios passed!" % (
+        total_result.scenarios_passed,
+        total_result.scenarios_ran
+    )
+
+
+# web steps
+@step(r'I fill in "(.*)" with "(.*)"')
+def fill_field(step, label, value):
+    world.browser.fill_in(label, value)
+
+@step(r'I press "(.*)"')
+def press_button(step, name):
+    world.browser.find_by_name(name.lower()).click()
+
+@step(r'I should see "(.*)": "(.*)"')
+def see_data_news(step, label, value):
+    tag = world.browser.find_by_id(label.lower()).value
+    result = "%s: %s" % (label, value)
+    tag |should| equal_to(result)
+
+@step(r'I should see the message "(.*)"')
+def see_delete_news_message(step, value):
+    tag = world.browser.find_by_css_selector('h1').value
+    tag |should| equal_to(value)
+
