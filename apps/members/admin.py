ccc902f2259ae90fe7991e6707a7b92154315b39
Ronaldo Amaral <rsantos@iff.edu.br>
2011-03-02 17:22:20 -0300

    added Participation InLine in Member Admin

--- apps/members/admin.py
+++ apps/members/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from apps.members.models import Member
+from apps.members.models import Participation
 
-admin.site.register(Member)
+
+class ParticipationInline(admin.TabularInline):
+    model = Participation
+    extra = 1
+
+class MemberAdmin(admin.ModelAdmin):
+    inlines = [ParticipationInline]
+
+
+admin.site.register(Member, MemberAdmin)
