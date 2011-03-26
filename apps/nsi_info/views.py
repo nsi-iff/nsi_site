d2b598de5284aa67c8ae9c17d8673ae7eba941ca
Herman Caldara <hermancaldara@gmail.com>
2011-03-26 01:14:16 -0300

    Fixed the problem in nsi_info views. An exception was raised when trying to acess the history or summary page if there isn't some nsi_info on database.

--- apps/nsi_info/views.py
+++ apps/nsi_info/views.py
@@ -4,17 +4,25 @@ from apps.nsi_info.models import NSIInfo
 
 
 def show_history(request):
+    if len(NSIInfo.objects.all()):
+        nsi_info = NSIInfo.objects.latest('updated_at')
+    else:
+        nsi_info = list()
     return render_to_response(
         'show_history.html',
-        {'nsi_info': NSIInfo.objects.all()[0]},
+        {'nsi_info': nsi_info},
         context_instance=RequestContext(request)
     )
 
 
 def show_summary(request):
+    if len(NSIInfo.objects.all()):
+        nsi_info = NSIInfo.objects.latest('updated_at')
+    else:
+        nsi_info = list()
     return render_to_response(
         'show_summary.html',
-        {'nsi_info': NSIInfo.objects.all()[0]},
+        {'nsi_info': nsi_info},
         context_instance=RequestContext(request)
     )
 
