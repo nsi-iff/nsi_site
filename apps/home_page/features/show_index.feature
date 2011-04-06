c396f7de94adba4e396ef9808afb5307bf88487e
Herman Caldara <hermancaldara@gmail.com>
2011-04-06 10:55:25 -0300

    Modified the icons of home page and made some layout improvements

--- apps/home_page/features/show_index.feature
+++ apps/home_page/features/show_index.feature
@@ -3,19 +3,31 @@ Feature: User visitor
   I want see home index data
 
   Scenario: showing News in index home
+    Given exist a author:
+      | name |
+      | herman |
+      | ronaldo |
     Given exist a news:
-      | title    | summary |
-      | Notícia sobre o NSI Site | Esta é uma noticia sobre o NSI Site | 
-      | Notícia sobre o ERP5 Lek | Esta é uma noticia sobre o ERP5 Lek |
-      | Notícia sobre o ERP5 e o NSI Site | Esta é uma noticia sobre o ERP5 e o NSI Site |
+      | title    | image | author | date_and_time |
+      | Notícia sobre o NSI Site | test/images/news/nsi.png | herman | 15/01/2011 20:00 |
+      | Notícia sobre o ERP5 Lek | test/images/news/erp5.png | herman | 05/04/2011 20:00 |
+      | Notícia sobre o ERP5 e o NSI Site | test/images/news/x.png | ronaldo | 01/01/2010 20:00 |
         
     When I go to "the NSI home page"
-    And I should see "Notícia sobre o NSI Site "
-    And I should see "Esta é uma noticia sobre o NSI Site"
+    And I should see "Notícia sobre o NSI Site"
+    And I should see an image called "nsi.90x90.png"
+    And I should see "Postado por: herman"
+    And I should see "15/01/2011"
+
     And I should see "Notícia sobre o ERP5 Lek"
-    And I should see "Esta é uma noticia sobre o ERP5 Lek"
+    And I should see an image called "erp5.90x90.png"
+    And I should see "Postado por: herman"
+    And I should see "05/04/2011"
+
     And I should see "Notícia sobre o ERP5 e o NSI Site"
-    And I should see "Esta é uma noticia sobre o ERP5 e o NSI Site"
+    And I should see an image called "x.90x90.png"
+    And I should see "Postado por: ronaldo"
+    And I should see "01/01/2010"
     
   Scenario: showing a open Projects in index home
     Given exist a project:
--- apps/home_page/features/steps.py
+++ apps/home_page/features/steps.py
@@ -1,25 +1,44 @@
 # -*- coding: utf-8 -*-
+import os
+import shutil
+from datetime import datetime
+from django.conf import settings
 from lettuce import step
-from model_mommy.mommy import Mommy
+from model_mommy import mommy
 from apps.news.models import News
 from apps.projects.models import Project
 from apps.tools.models import Tool
+from django.contrib.auth.models import User
+
+
+@step(u'exist a author:')
+def exist_a_author(step):
+    for user in step.hashes:
+        mommy.make_one(User, username=user.get('name'), email='a@a.com')
 
 @step(u'exist a news:')
 def exist_a_news(step):
     for news_hashes in step.hashes:
-        mom = Mommy(News, False)
-        news = mom.make_one(title=news_hashes.get('title'), summary=news_hashes.get('summary'))
-        
+        author = User.objects.get(username__exact=news_hashes.get('author'))
+        mom = mommy.Mommy(News, False)
+        date, hour = news_hashes.get('date_and_time').split()
+        day, month, year = date.split('/')
+        hours, minutes = hour.split(':')
+        date_time = datetime(int(year), int(month), int(day), int(hours), int(minutes))
+        news = mom.make_one(title=news_hashes.get('title'), image=news_hashes.get('image'), author=author, date_and_time=date_time).save()
+        file_name = news_hashes.get('image').split('/')[-1]
+        shutil.copy2(os.path.join(settings.PROJECT_ROOT_PATH, 'apps', 'home_page',
+                                  'features', 'resources', file_name),
+                     os.path.join(settings.MEDIA_ROOT, 'test', 'images', 'news'))
 
 @step(u'exist a project:')
 def exist_a_project(step):
     for project_hashes in step.hashes:
-        mom = Mommy(Project, False)
+        mom = mommy.Mommy(Project, False)
         project = mom.make_one(name=project_hashes.get('name'), status=project_hashes.get('status'))
         
 @step(u'exist a tool:')
 def exist_a_tool(step):
     for tool_hashes in step.hashes:
-        mom = Mommy(Tool, False)
+        mom = mommy.Mommy(Tool, False)
         tool = mom.make_one(name=tool_hashes.get('name'), highlight=tool_hashes.get('highlight'))
