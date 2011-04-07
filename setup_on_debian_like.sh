ede6175496330ed7ffad1512fd6f1003411e112e
Vinícius das Chagas Silva <vinimaster@gmail.com>
2011-04-06 22:47:29 -0300

    Added libjpeg62-dev to provide support to jpeg by PIL

--- setup_on_debian_like.sh
+++ setup_on_debian_like.sh
@@ -1,3 +1,4 @@
-sudo apt-get install python-dev libxml2 libxslt1.1 libxslt1-dev
+sudo apt-get install python-dev libxml2 libxslt1.1 libxslt1-dev libjpeg62-dev
 sudo apt-get install python-setuptools
 easy_install pip
+
