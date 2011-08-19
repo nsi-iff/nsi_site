203379d840c6966697781f703ef94609cbb476b4
Herman Caldara <hermancaldara@gmail.com>
2011-08-19 13:25:27 -0300

    Moved wiki urls from urls.py base to urls.py inside wiki app

--- urls.py
+++ urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
-from django.views.generic.create_update import update_object, delete_object
 from django.conf import settings
 from django.contrib import admin
-
-from apps.wiki.models import WikiItem
 
 
 admin.autodiscover()
@@ -31,21 +27,7 @@ urlpatterns = patterns('',
     (r'^ferramentas/$', 'apps.tools.views.show_all'),
     (r'^ferramenta/(?P<tool_slug>[\w_-]+)$', 'apps.tools.views.show_tool'),
 
-    (r'^wiki/$', 'apps.wiki.views.show_all_wiki_items'),
-    (r'^wiki/(?P<wiki_item_id>\d+)/$', 'apps.wiki.views.view_wiki_item'),
-    (r'^wiki/(?P<object_id>\d+)/editar/$', update_object,
-        {'model': WikiItem, 'template_name': 'edit_wiki_item.html',
-        'post_save_redirect': '/wiki/item_editado_com_sucesso/'}),
-    (r'^wiki/item_editado_com_sucesso/$',
-        direct_to_template, {'template': 'wiki_item_successfully_updated.html'}),
-    (r'^wiki/(?P<object_id>\d+)/excluir/$', delete_object,
-        {'model': WikiItem, 'template_name': 'delete_wiki_item.html',
-        'post_delete_redirect': '/wiki/item_excluido_com_sucesso/'}),
-    (r'^wiki/item_excluido_com_sucesso/$', 
-        direct_to_template, {'template': 'wiki_item_successfully_deleted.html'}),
-    (r'^wiki/novo_item/$', 'apps.wiki.views.add_wiki_item'),
-    (r'^wiki/novo_item/adicionado_com_sucesso/$',
-        direct_to_template, {'template': 'wiki_item_successfully_added.html'}),
+    (r'^wiki/', include('apps.wiki.urls')),
 
     (r'^site_media/(.*)$', 'django.views.static.serve', {"document_root": settings.MEDIA_ROOT}),
 )
