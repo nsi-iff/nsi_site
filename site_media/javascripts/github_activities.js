1bd14b0ded17cc8606906c6aa2c21d7089b39c26
Gabriel L. Oliveira <ciberglo@gmail.com>
2011-10-04 23:43:28 -0300

    fix to not show github activities when there's no activity #54

--- site_media/javascripts/github_activities.js
+++ site_media/javascripts/github_activities.js
@@ -10,20 +10,22 @@ function parseRSS(url, callback) {
 }
 
 function placeActivities(feed) {
-  $('div.github_activities span.text_highlight').show();
-  var div_activities = $('ul.activities');
-  $(feed.entries).each(function(key, entry){
-    var author = entry.author;
-    var title = entry.title;
-    var link = entry.link;
-    //remove author from start of entry title
-    var author_regexp = new RegExp("^" + author + " ");
-    title = title.replace(author_regexp, '');
-    var div_entry = $('<a />').append($('<li />').text(title))
-    div_entry.attr('href', link);
-    div_entry.attr('target', '_blank');
-    div_activities.append(div_entry);
-  });
+  if (feed.entries.length > 0) {
+    $('div.github_activities span.text_highlight').show();
+    var div_activities = $('ul.activities');
+    $(feed.entries).each(function(key, entry){
+      var author = entry.author;
+      var title = entry.title;
+      var link = entry.link;
+      //remove author from start of entry title
+      var author_regexp = new RegExp("^" + author + " ");
+      title = title.replace(author_regexp, '');
+      var div_entry = $('<a />').append($('<li />').text(title))
+      div_entry.attr('href', link);
+      div_entry.attr('target', '_blank');
+      div_activities.append(div_entry);
+    });
+  }
 }
 
 $(document).ready(function(){
