3d1fadd5ebf924e1e91b747f0cdb8e316f2dda22
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2011-11-26 22:01:09 -0200

    add members in context

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -15,10 +15,20 @@ def show_all(request):
 def show_project(request, project_slug):
     project = Project.objects.get(slug=project_slug)
     participations = Participation.objects.all()
+
+    project_members = []
+
+    for participation in participations:
+        if participation.project.name in project.name:
+            project_members.append(participation.member)
+
     return render_to_response(
         'show_project.html',
-        {'project': project, 'participations': participations},
+        {
+            'project': project,
+            'participations': participations,
+            'project_members': project_members
+        },
         context_instance=RequestContext(request)
     )
-        
 
