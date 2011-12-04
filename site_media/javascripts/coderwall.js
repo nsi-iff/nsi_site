3afb0a21d7274dd15316954cdeb255efa862b92f
Rômulo Machado <romulo.machado5@gmail.com>
2011-12-04 00:53:30 -0200

    Made coderwall script work with new layout

--- site_media/javascripts/coderwall.js
+++ site_media/javascripts/coderwall.js
@@ -3,13 +3,14 @@
 */
 
 $(document).ready(function(){
-        var member = $('#github_user').text();
+        var github_link = $('#github_user').text().split('/');
+        var member = github_link[github_link.length-1]
         var coderwallJSONurl ="http://www.coderwall.com/" + member + ".json?callback=?";
         var height = 75;
         var width = 75;
 
         $.getJSON(coderwallJSONurl, function(data) {
-        $("<p><a href='http://coderwall.com/" + member + "' id='coderwall_link'>Coderwall Achievements</a></p> <br /><br />").appendTo("#coderwall");
+        $("<p><strong><a href='http://coderwall.com/" + member + "' id='coderwall_link'>Coderwall Achievements</a></strong></p>").appendTo(".coderwall");
           $.each(data.data.badges, function(i, item) {
             $("<img/>").attr("src", item.badge)
               .attr("float", "left")
@@ -17,7 +18,7 @@ $(document).ready(function(){
               .attr("alt", item.name)
               .attr("height", height)
               .attr("width", width)
-              .appendTo("#coderwall")
+              .appendTo(".coderwall")
               .hover(
                   function(){
                       $(this).css("opacity","0.6");
