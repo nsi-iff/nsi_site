4b75ca952d5fc1ffc3c7d942afee117b29616841
Rômulo Machado <romulo.machado5@gmail.com>
2011-12-04 18:10:08 -0200

    Optimized coderwall script. Thanks @bernardofire

--- site_media/javascripts/coderwall.js
+++ site_media/javascripts/coderwall.js
@@ -12,22 +12,24 @@ $(document).ready(function(){
         $.getJSON(coderwallJSONurl, function(data) {
         $("<p><strong><a href='http://coderwall.com/" + member + "' id='coderwall_link'>Coderwall Achievements</a></strong></p>").appendTo(".coderwall");
           $.each(data.data.badges, function(i, item) {
-            $("<img/>").attr("src", item.badge)
-              .attr("float", "left")
-              .attr("title", item.name + ": " + item.description)
-              .attr("alt", item.name)
-              .attr("height", height)
-              .attr("width", width)
-              .appendTo(".coderwall")
-              .hover(
-                  function(){
-                      $(this).css("opacity","0.6");
-                  },
-                  function(){
-                      $(this).css("opacity","1.0");
-                  }
-              )
-            });
+            $("<img/>").attr({
+				"src": item.badge,
+				"float": "left",
+				"title": item.name + ": " + item.description,
+				"alt": item.name,
+				"height": height,
+				"width": width 
+			})
+            .appendTo(".coderwall")
+            .hover(
+				function(){
+					$(this).css("opacity","0.6");
+                },
+                function(){
+                    $(this).css("opacity","1.0");
+                }
+            )
         });
+    });
 });
 
