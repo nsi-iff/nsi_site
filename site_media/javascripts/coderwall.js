f340d5faf7cab35911943292b174f0a2e9ceaba0
Rômulo Machado <romulo.machado5@gmail.com>
2011-12-04 18:18:05 -0200

    Removed tabs, put spaces instead

--- site_media/javascripts/coderwall.js
+++ site_media/javascripts/coderwall.js
@@ -13,17 +13,17 @@ $(document).ready(function(){
         $("<p><strong><a href='http://coderwall.com/" + member + "' id='coderwall_link'>Coderwall Achievements</a></strong></p>").appendTo(".coderwall");
           $.each(data.data.badges, function(i, item) {
             $("<img/>").attr({
-				"src": item.badge,
-				"float": "left",
-				"title": item.name + ": " + item.description,
-				"alt": item.name,
-				"height": height,
-				"width": width 
-			})
+                "src": item.badge,
+                "float": "left",
+                "title": item.name + ": " + item.description,
+                "alt": item.name,
+                "height": height,
+                "width": width 
+            })
             .appendTo(".coderwall")
             .hover(
-				function(){
-					$(this).css("opacity","0.6");
+                function(){
+                    $(this).css("opacity","0.6");
                 },
                 function(){
                     $(this).css("opacity","1.0");
